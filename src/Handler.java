52b8f7ab9319971e8f8045f27fd0a4ef6960b164
pr0x79 <94390613+pr0x79@users.noreply.github.com>
2021-11-29 15:41:54 +0100

    testat 3 handler klasse
    handler erstellt, wird aufgerufen wenn im main menu 3 eingegeben wird, muss aber noch printoffices und geteventlog implementieren

--- src/Handler.java
+++ src/Handler.java
@@ -1,10 +1,14 @@
-public class Handler {
+public class Handler{
 public static void main(String[] args) {
     System.out.println("Ich bins der Handler");
+    
+
+    
  
 /*  Platzhalter
 
     getEventLog();
+    Printoffices;
 }
 
 private static void getEventLog() {
--- src/PrintOffices.java
+++ src/PrintOffices.java
@@ -1,14 +1,11 @@
 import gmbh.kdb.hsw.gdp.domain.GameDevStudio;
-import gmbh.kdb.hsw.gdp.domain.Office;
 
 public class PrintOffices implements Command{
 
-    Office office1 = new Office(null, null);
-    
-    
     @Override
     public Command execute(GameDevStudio studio) {
-        System.out.println("Offices:");
+        System.out.printf("Name= %s \n,",
+        studio.getOffices().get(0).getName().getName());
         return null;
     }
     
