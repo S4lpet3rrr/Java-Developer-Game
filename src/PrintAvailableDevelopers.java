032d1c0d9af586339bcf43a17ffd21fbcb9bc32e
DamianBender <57224031+DamianBender@users.noreply.github.com>
2021-12-12 14:39:44 +0100

    Update PrintAvailableDevelopers.java

--- src/PrintAvailableDevelopers.java
+++ src/PrintAvailableDevelopers.java
@@ -1,6 +1,7 @@
 import gmbh.kdb.hsw.gdp.domain.GameDevStudio;
 import gmbh.kdb.hsw.gdp.domain.Application;
 import gmbh.kdb.hsw.gdp.domain.Developer;
+import gmbh.kdb.hsw.gdp.domain.Money;
 import java.util.List;
 
 public class PrintAvailableDevelopers implements Command {
@@ -9,8 +10,12 @@ public class PrintAvailableDevelopers implements Command {
         System.out.println("Available Developers");
         List<Application> apps = studio.getApplications();
         for(int i = 0 ; i < apps.size() ; i++){
-            System.out.println(apps.get(i).toString());
+            Application app = apps.get(i);
+            System.out.println(app.toString());
+            Money remainCash = studio.getCash().subtract(app.getHireAgentFee());
+            System.out.println("Remain" + remainCash);
         }
+
         return new DisplayMainMenu();
     }
     
