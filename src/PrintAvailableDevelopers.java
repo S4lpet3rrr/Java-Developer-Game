651790ecf423e783c1aa064a08e47abb08eb069f
S4lpet3rrr <68060643+S4lpet3rrr@users.noreply.github.com>
2021-12-12 13:13:20 +0100

    base für heute testat 2

--- src/PrintAvailableDevelopers.java
+++ src/PrintAvailableDevelopers.java
@@ -1,4 +1,12 @@
+import gmbh.kdb.hsw.gdp.domain.GameDevStudio;
+import gmbh.kdb.hsw.gdp.domain.Developer;
+import java.util.List;
+
 public class PrintAvailableDevelopers implements Command {
+    @Override
+    public Command execute(GameDevStudio studio){
+
+    }
     
     
 
--- src/PrintAvailableProjects.java
+++ src/PrintAvailableProjects.java
@@ -1,7 +1,12 @@
+import gmbh.kdb.hsw.gdp.domain.GameDevStudio;
 import gmbh.kdb.hsw.gdp.domain.Developer;
+import java.util.List;
 
 public class PrintAvailableProjects implements Command {
-
+    @Override
+    public Command execute(GameDevStudio studio){
+        
+    }
     
     
 }
