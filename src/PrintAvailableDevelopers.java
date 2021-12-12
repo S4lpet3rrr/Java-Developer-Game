8303fa8c663986a6bd442d701ea32d10e262b7b5
DamianBender <57224031+DamianBender@users.noreply.github.com>
2021-12-12 13:48:49 +0100

    available developers

--- src/PrintAvailableDevelopers.java
+++ src/PrintAvailableDevelopers.java
@@ -1,11 +1,17 @@
 import gmbh.kdb.hsw.gdp.domain.GameDevStudio;
+import gmbh.kdb.hsw.gdp.domain.Application;
 import gmbh.kdb.hsw.gdp.domain.Developer;
 import java.util.List;
 
 public class PrintAvailableDevelopers implements Command {
     @Override
     public Command execute(GameDevStudio studio){
-
+        System.out.println("Available Developers");
+        List<Application> apps = studio.getApplications();
+        for(int i = 0 ; i < apps.size() ; i++){
+            System.out.println(apps.get(i).toString());
+        }
+        return new DisplayMainMenu();
     }
     
     
